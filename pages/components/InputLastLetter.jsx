eba8fd79863d408fc804fdd2ff2588adc7a3349b
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 11:06:08 +0100

    esqueleto input y botón

--- pages/components/InputLastLetter.jsx
+++ pages/components/InputLastLetter.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const InputLastLetter = () => {
   return (
+    <>
     <div>InputLastLetter</div>
+    <br></br>
+    <input onChange={(e)=>{console.log(e.target.value)}}></input>
+    <button onClick={()=>{}}>Pon la última letra en mayúsculas</button>
+    </>
   )
 }
 
