85f0104995b0b763d4e094aa7737e41b3ae6ff87
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 17:22:20 +0100

    lógica contar palabras split(" ") .length
     const counterWords = (value)=>{
            setInputUser(value)
            const lengthWord = value.split(" ").length
            setNumberWords(lengthWord)
        }

--- pages/components/WordsCounter.jsx
+++ pages/components/WordsCounter.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
 
 const WordsCounter = () => {
-    
-    const [inputUser, setInputUser] = useState()
+
+    const [inputUser, setInputUser] = useState([]);
+
+    const [numberWords, setNumberWords] = useState(0);
+
+    const counterWords = (value)=>{
+        setInputUser(value)
+        const lengthWord = value.split(" ").length 
+        setNumberWords(lengthWord)
+    }
 
 
   return (
     <>
     <div>WordsCounter</div>
-    <input onChange={(e)=>{setInputUser(e.target.value)}}></input>
+    <input onChange={(e)=>{counterWords(e.target.value)}}></input>
     {inputUser}
+    {numberWords}
     </>
   )
 }
