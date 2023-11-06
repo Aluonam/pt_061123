c83a9d7cc5e9703e0407e9834317814accf61319
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 17:05:59 +0100

    esqueleto input

--- pages/components/WordsCounter.jsx
+++ pages/components/WordsCounter.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const WordsCounter = () => {
+    
+    const [inputUser, setInputUser] = useState()
+
+
   return (
+    <>
     <div>WordsCounter</div>
+    <input onChange={(e)=>{setInputUser(e.target.value)}}></input>
+    {inputUser}
+    </>
   )
 }
 
