aa35e65a8f13e51f2eb4b44a4c3cc40e80b8ad29
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 10:52:51 +0100

    consumoAPI en consola

--- pages/components/TableDataAPI.jsx
+++ pages/components/TableDataAPI.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const TableDataAPI = () => {
+
+    const [dataAPI, setDataAPI] = useState()
+
+    useEffect(() => {
+      const apiUrl = `https://randomuser.me/api/?results=50`;
+      fetch(apiUrl) 
+      .then(response => response.json())
+      .then(data=>console.log(data.results))
+      .catch(error => console.log("error detected", error))
+      
+    }, [])
+    
+
   return (
     <div>TableDataAPI</div>
   )
