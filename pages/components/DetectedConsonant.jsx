fb7fde430fd3ce297eac221d83afe5d2f72c3883
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 16:57:58 +0100

    lógica  split join split filter some toLoweCase if
      const handleDetectedConsonant = (value)=>{
            setInputText(value)
    
            const vocals = ["a", "e", "i", "o", "u"];
            const letterByLetter = value.split(" ").join("").split("")
            let numberConsonants = 0
            letterByLetter.filter((letter)=>{
                    const isVocal = vocals.some((element)=> element==letter.toLowerCase())
                    if (!isVocal){numberConsonants++}
                    }
            )
    
            console.log(letterByLetter)
            console.log("consonantes: ", numberConsonants)
        }

--- pages/components/DetectedConsonant.jsx
+++ pages/components/DetectedConsonant.jsx
@@ -2,14 +2,29 @@ import React, { useState } from 'react'
 
 const DetectedConsonant = () => {
     const [inputText, setInputText] = useState("")
+
+    const handleDetectedConsonant = (value)=>{
+        setInputText(value)
+
+        const vocals = ["a", "e", "i", "o", "u"];
+        const letterByLetter = value.split(" ").join("").split("")
+        let numberConsonants = 0
+        letterByLetter.filter((letter)=>{
+                const isVocal = vocals.some((element)=> element==letter.toLowerCase())
+                if (!isVocal){numberConsonants++}
+                } 
+        )
+
+        console.log(letterByLetter)
+        console.log("consonantes: ", numberConsonants)
+    }
   return (
     <>
     <br/>
     <div>Escribe para detectas las consonantes</div>
     <br/>
-    <input onChange={(e)=>{setInputText(e.target.value)}}></input>
+    <input onChange={(e)=>{handleDetectedConsonant(e.target.value)}}></input>
     <br/>
-    <button onClick={()=>{}}>Detectar</button>
     {inputText}
     </>
   )
