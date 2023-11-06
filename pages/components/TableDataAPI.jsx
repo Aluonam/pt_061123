329f1b352f659a20b3bfc8dd9983c399395838ae
Paula Cano <paulacanoroman@gmail.com>
2023-11-06 10:56:36 +0100

    tabla formato básico sin datos

--- pages/components/TableDataAPI.jsx
+++ pages/components/TableDataAPI.jsx
@@ -7,15 +7,39 @@ const TableDataAPI = () => {
     useEffect(() => {
       const apiUrl = `https://randomuser.me/api/?results=50`;
       fetch(apiUrl) 
-      .then(response => response.json())
-      .then(data=>console.log(data.results))
-      .catch(error => console.log("error detected", error))
+            .then(response => response.json())
+            // .then(data=>console.log(data.results))
+            .then(data=>setDataAPI(data.results))
+            .catch(error => console.log("error detected", error))
       
     }, [])
-    
+
+
 
   return (
+    <>
     <div>TableDataAPI</div>
+    <table>
+        <thead>
+            <tr>
+                <td>Nombre y apellidos</td>
+                <td>Nombre de usuario</td>
+                <td>Contraseña</td>
+                <td>Email</td>
+                <td>Teléfono</td>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+    </>
   )
 }
 
